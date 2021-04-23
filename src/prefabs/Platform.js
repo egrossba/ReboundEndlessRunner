3b776c431156755372dda9f5fb451d7919b63221
Ethan Grossbach <egrossba@ucsc.edu>
2021-04-22 18:11:22 -0700

    immovable

--- src/prefabs/Platform.js
+++ src/prefabs/Platform.js
@@ -3,6 +3,7 @@ class Platform extends Phaser.GameObjects.Sprite {
         super(scene, x, y, texture, frame);
         this.p1 = scene.physics.add.sprite(game.config.width/2, game.config.height/2, texture, frame).setScale(SCALE).setOrigin(0);
         this.p1.setScale(1);
+        this.p1.body.immovable = true;
         this.p1.body.allowGravity = false;
         this.p1.body.setMaxVelocity(MAX_X_VEL, MAX_Y_VEL);
         this.p1.body.setCollideWorldBounds(true);
